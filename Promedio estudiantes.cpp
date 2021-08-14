1635628e4963912f46036a05c093100f0fa0b459
Johann <87659137+Chorizo1414@users.noreply.github.com>
2021-08-13 22:23:13 -0600

    Add files via upload

--- Promedio estudiantes.cpp
+++ Promedio estudiantes.cpp
@@ -5,35 +5,25 @@ using namespace std;
 
 int main()
 {
-	float n;
-	float notas[10];
-//	float nota1, nota2, nota3, nota4, nota5, nota6, nota7, nota8, nota9, nota10, nota11, nota12, nota13, nota14, nota15;
-	float promf;
-	
-	
+	int nota, i, n, promf;
+	char opcion[3];
+
 	do {
-			cout<<"Ingrese la cantidad de notas a ingresar:";
-			cin>>("%n", n);
-		
-			for (int i=0; i < n; i++)
-			{
-				cout<<("Ingrese la nota %n", i+1);
-				cin>>("%d", notas[i]);
-			}
-			promf=notas/n
-			printf("El promedio es %.1f\notas"<<promf<<endl;
-			
-			return 0;	
-        }
 
+		cout << "Ingrese la cantidad de notas a ingresar:";
+		cin >> ("%n", n);
 
+		for (i = 0; i < n; i++) //dependiendo la cantidad ingresada pedira las notas a ingresas
+		{
+			cout << "Ingrese la nota:";
+			cin >> nota;
+		}
+		promf = nota / n;
+		cout<<"El promedio es:" << promf << endl;
 
+			cout << "Desea calcular mas promedios (si/no)?";
+			cin >> ("%s", opcion);
 
-
-//	cin>>nota1;
-//	cout<<"Ingrese la nota 2:";
-//	cin>>nota2;
-//	promf=(nota1 + nota2)/2;
-//	cout<<"su promedio final es:"<<promf<<endl;
+	} while (opcion[0] == 's' || opcion[0] == 'S');
+	return 0;
 }
-
